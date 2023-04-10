b747f3c53f2cc4308865e465100afa6a28c212df
JP Sauve <jean-paul@grokology.ca>
2023-04-10 14:15:25 -0400

    first commit

---
+++ src/test/kotlin/ca/grokology/blog/BlogApplicationTests.kt
@@ -0,0 +1,13 @@
+package ca.grokology.blog
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
